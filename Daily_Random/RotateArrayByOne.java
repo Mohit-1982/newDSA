602b37f9f4fa9b4c383b626107a51970f8a3b353
Mohit <mohitdagar2k08@gmail.com>
2026-05-02 15:09:22 +0530

    Create RotateArrayByOne.java

---
+++ Daily_Random/RotateArrayByOne.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void rotate(int[] arr) {
+        for (int i = arr.length - 1; i > 0; i--) {
+            int temp = arr[i];
+            arr[i] = arr[i - 1];
+            arr[i - 1] = temp;
+        }
+    }
+}
