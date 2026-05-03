b3a4ff62ae0f8877e543d7b19940582952fe7b03
Mohit <mohitdagar2k08@gmail.com>
2026-05-03 11:41:18 +0530

    Implement optimal missing number solution
    Added an optimal solution for finding the missing number in an array.

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -45,3 +45,18 @@ Better :
         return n;
     }
 }
+
+Optimal : 
+  class Solution {
+    public int missingNumber(int[] nums) {
+        int currSum = 0;
+        int n = nums.length;
+        int sum = (n * (n + 1)) / 2;
+
+        for (int i = 0; i < n; i++) {
+            currSum += nums[i];
+        }
+
+        return sum - currSum;
+    }
+}
