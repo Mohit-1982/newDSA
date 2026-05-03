392c3947c8aa9f46834d370350211a8f9a4f7864
Mohit <mohitdagar2k08@gmail.com>
2026-05-03 11:37:37 +0530

    Create MissingNumber.java

---
+++ Arrays/MissingNumber.java
@@ -0,0 +1,47 @@
+Leetcode - 268
+  Brute : 
+    class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n; j++) {
+                if (nums[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) return i;
+        }
+
+        return n;
+    }
+}
+
+Better : 
+  class Solution {
+    public int missingNumber(int[] nums) {
+        int i = 0;
+        int n = nums.length;
+
+        while (i < n) {
+            if (nums[i] == i || nums[i] >= n) {
+                i++;
+            }else {//not at its correct place
+                int temp = nums[i];
+                nums[i] = nums[nums[i]];
+                nums[temp] = temp;
+            }
+        }
+
+        for (i = 0; i < n; i++) {
+            if (nums[i] != i) {
+                return i;
+            }
+        }
+
+        return n;
+    }
+}
