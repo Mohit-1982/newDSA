08ff49cecb24aa5c249e5192f5d0164e57d87c61
Mohit <mohitdagar2k08@gmail.com>
2026-05-24 15:26:54 +0530

    Implement max subarray sum of size k

---
+++ Arrays/PrefixSum + HashMap/MaxSubarrayOfSizeK.java
@@ -0,0 +1,24 @@
+GFG
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        int sum = 0;
+        int n = arr.length;
+        
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        
+        int i = 0;
+        int j = k;
+        int maxSum = sum;
+        
+        while (j < n) {
+            sum = sum + arr[j] - arr[i];
+            maxSum = Math.max(sum, maxSum);
+            j++;
+            i++;
+        }
+        
+        return maxSum;
+    }
+}
