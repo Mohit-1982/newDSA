db6ca8efb10f0f8ff9a89c71f04c653217dc92c2
Mohit <mohitdagar2k08@gmail.com>
2026-05-27 15:08:53 +0530

    Create MaximumSubarray.java

---
+++ Arrays/Kadane's Algorithm/MaximumSubarray.java
@@ -0,0 +1,23 @@
+Leetocde - 53
+  Brute : 
+    class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+
+                if (sum > maxSum) maxSum = sum;
+            }
+        }
+
+        return maxSum;
+    }
+}
+
+Optimal : 
+  
