01ff9d42fbc0b4db13c844d9d68bda656e41d0d5
Mohit <mohitdagar2k08@gmail.com>
2026-06-24 11:42:01 +0530

    Implement upper bound function in Java

---
+++ BINARY_SEARCH/ImplementUpperBound.java
@@ -0,0 +1,20 @@
+GFG
+  Optimal :   
+    class Solution {
+    int upperBound(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length - 1;
+        
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+            
+            if (arr[m] > target) {
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+        
+        return s;
+    }
+}
