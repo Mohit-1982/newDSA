15cfbb857f07fd474d2d50c09197ebe406871728
Mohit <mohitdagar2k08@gmail.com>
2026-07-09 22:14:26 +0530

    Implement Sieve of Eratosthenes algorithm

---
+++ SieveOfEratosthenes/SieveOfEratosthenes.java
@@ -0,0 +1,28 @@
+GFG
+  Optimal : 
+    class Solution {
+    public ArrayList<Integer> sieve(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * 2; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        ArrayList<Integer> res = new ArrayList<>();
+        
+        for (int i = 0; i <= n; i++) {
+            if (isPrime[i]) {
+                res.add(i);
+            }    
+        }
+        
+        return res;
+    }
+}
