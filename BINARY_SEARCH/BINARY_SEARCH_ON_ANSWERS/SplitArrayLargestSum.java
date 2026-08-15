4450d6f97cb76fe1c24f6404e544e21d93a70613
Mohit <mohitdagar2k08@gmail.com>
2026-08-15 10:54:32 +0530

    Add SplitArrayLargestSum.java for Leetcode 410
    Implement binary search solution for splitting array.

---
+++ BINARY_SEARCH/BINARY_SEARCH_ON_ANSWERS/SplitArrayLargestSum.java
@@ -0,0 +1,65 @@
+Leetcode - 410
+  Optimal: 
+    class Solution {
+    public int splitArray(int[] nums, int k) {
+        int n = nums.length;
+
+        if (k > n) return -1;
+
+        long s = max(nums);
+        long e = sum(nums);
+        long res = Long.MAX_VALUE;
+
+        while (s <= e) {
+            long m = s + (e - s) / 2;
+            long larSum = isPossible(nums, k, m);
+
+            if (larSum != -1) {
+                res = Math.min(res, larSum);
+                e = m - 1;
+            } else {
+                s = m + 1;
+            }
+        }
+
+        return (int) res;
+    }
+
+    public long isPossible(int[] arr, int arraysToCreate, long capacity) {
+        long lar = Long.MIN_VALUE;
+        int currSubArrays = 1;
+        long currCapacity = 0;
+
+        for (int ele : arr) {
+            if (ele + currCapacity > capacity) {
+                currCapacity = ele;
+                currSubArrays++;
+
+                if (currSubArrays > arraysToCreate) 
+                    return -1;
+            } else {
+                currCapacity += ele;
+            }
+
+            lar = Math.max(lar, currCapacity);
+        }
+
+        return lar;
+    }
+
+    public long sum(int[] arr) {
+        long sum = 0;
+
+        for (int ele : arr) sum += ele;
+
+        return sum;
+    }
+
+    public int max(int[] arr) {
+        int max = 0;
+
+        for (int ele : arr) max = Math.max(max, ele);
+
+        return max;
+    }
+}
