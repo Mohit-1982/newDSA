eec6f6b3a804cac1b0495dcfd22b98542f58c0d2
Mohit <mohitdagar2k08@gmail.com>
2026-09-14 17:15:23 +0530

    Add RemoveNthNodeFromEndOfLinkedList solution
    Implement solution for removing the N-th node from the end of a linked list.

---
+++ LinkedList/SinglyLinkedList/RemoveNthNodeFromEndOfLinkedList.java
@@ -0,0 +1,25 @@
+Leetcode - 19
+  Optimal:
+    class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null || head.next == null) return null;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+
+        if (fast == null) return head.next;
+
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+}
